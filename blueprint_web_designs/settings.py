224aae628b1afeec51d168c8a53f9ebd2858319f
paulfitzpatrick85 <fitzer99@gmail.com>
2022-10-01 15:52:52 +0000

    navbar change

--- blueprint_web_designs/settings.py
+++ blueprint_web_designs/settings.py
@@ -149,7 +149,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # tells django where all static files are located
+
+
+MEDIA_URL = '/media/'             
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')    #where all uploaded media files will go
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
