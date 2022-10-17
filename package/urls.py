9525789dde83b633b5ce03477c5917afffbde920
paulfitzpatrick85 <fitzer99@gmail.com>
2022-10-17 21:15:53 +0000

    add package detail page

--- package/urls.py
+++ package/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('packages.html', views.all_packages, name='all_packages'),
+    path('package_detail.html/<package_id>/', views.package_detail, name='package_detail'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
--- package/views.py
+++ package/views.py
@@ -16,3 +16,15 @@ def all_packages(request):
     }
 
     return render(request, 'packages.html', context)
+
+
+def package_detail(request, package_id):
+    """ A view to return the package details page """
+
+    package = get_object_or_404(Package, pk=package_id)
+
+    context = {
+        'package': package,
+    }
+
+    return render(request, 'package_detail.html', context)
