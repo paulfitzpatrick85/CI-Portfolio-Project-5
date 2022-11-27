e88a8772008d1b90d6c22bde77514a772e068455
paulfitzpatrick85 <fitzer99@gmail.com>
2022-11-27 19:53:36 +0000

    debug set to true

--- blueprint_web_designs/settings.py
+++ blueprint_web_designs/settings.py
@@ -30,8 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-
+DEBUG = TRUE
 ALLOWED_HOSTS = ['ci-pp5-blueprint-webdesigns.herokuapp.com', 'localhost']
 
 
