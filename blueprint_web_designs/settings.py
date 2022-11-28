0bfd82c515b15c468158f554dbfa62cfe701eed7
paulfitzpatrick85 <fitzer99@gmail.com>
2022-11-28 22:08:25 +0000

    linking up s3 media test

--- blueprint_web_designs/settings.py
+++ blueprint_web_designs/settings.py
@@ -92,7 +92,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'cart.contexts.cart_contents'
+                'cart.contexts.cart_contents',
+                'django.template.context_processors.media',
             ],
             # 'builtins': [
             #     'crispy_forms.templatetags.crispy_forms_tags',
