e341298408e7d5541d414c6a9069df28c04896ed
paulfitzpatrick85 <fitzer99@gmail.com>
2022-12-07 20:49:25 +0000

    add email configs

--- blueprint_web_designs/settings.py
+++ blueprint_web_designs/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 ALLOWED_HOSTS = ['ci-pp5-blueprint-webdesigns.herokuapp.com', 'localhost']
 
